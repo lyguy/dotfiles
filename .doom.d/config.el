436a725510ee206a6062e9eacc28c97925563a59
Lyman B. Gillispie <lyman@lygi.me>
2021-03-26 09:09:35 +1100

    Update things for new doom emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,36 +1,71 @@
-;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Lyman Gillispie"
+      user-mail-address "lyman@lygi.me")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
 ;;
-(when (memq window-system '(mac ns x))
-  (setq exec-path-from-shell-check-startup-files nil)
-  (exec-path-from-shell-initialize))
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-solarized-light)
 
-(when (memq window-system '((message "")ac ns))
-  (setq exec-path-from-shell-check-startup-files nil)
-  (exec-path-from-shell-initialize))
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
 
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
 
+(setq deft-directory "~/org")
 (setq deft-recursive t)
 
-;; BibTex Setup
-;; The gist is: I use Zotero with better bibtex to manage the bibtex db,
-;; and files. All of the db management is left to Zotero, excpet notes,
-;; which we keep in the orgs.
-(defvar bibliographies (list "Online_Pajak.bib" "Math Books.bib"))
-(defvar research-path "~/org/research/")
+(setq auto-save-visited-mode t)
+(auto-save-visited-mode +1)
 
-(setq bibtex-completion-additional-search-fields '(tags keywords))
-(setq bibtex-completion-pdf-field "File")
-;; BibTex settings to help coordinate w/ Zotero
-;; Optionally specifying a location for the corresponding PDFs
-(setq  bibtex-completion-bibliography
-       (mapcar(lambda (bib) (concat research-path bib)) bibliographies))
-(setq bibtex-completion-notes-path (concat research-path "notes"))
+;; org-roam templates
+(after!
+  org-roam
+  (push
+   '("e" "contact" plain (function org-roam--capture-get-point)
+     "* Name: ${name}%?"
+     :file-name "contacts/${slug}"
+     :head "#+title: ${title}\n#+property: name ${name}\n#+property: nickname ${nickname}\n#+property: company ${company}\n#+property: team ${team}\n#+property: email ${email}\n#+property: phone ${phone}"
+     :unnarrowed t)
+   org-roam-capture-templates
+   ))
 
-(when (memq window-system '((message "")ac ns))
-  (setq exec-path-from-shell-check-startup-files nil)
-  (exec-path-from-shell-initialize))
-
-(setq doom-theme 'doom-solarized-light)
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
